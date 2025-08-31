3795d67e2b8d7ffa203b00c5f4d988b12621c02b
mitsuhaaa <mitsuhaaa8@outlook.com>
2025-08-31 23:52:16 +0800

    dpll v1.1 add time

--- main.c
+++ main.c
@@ -2,33 +2,39 @@
 #include <stdlib.h>
 #include "cnfparser.h"
 #include "solver.h"
-
+#include <time.h>
 int main()
 {
+    clock_t start, end;
+    double cpu_time_used;
     Formula formula;
     char filename[20];
     scanf("%s", filename);
     parse_cnf(filename, &formula);
     // print_formula(&formula);
     int *solution = (int *)malloc(sizeof(int) * (formula.variable_num + 1));
+    start = clock();
     if (dpll_solve(&formula, solution) == 1)
     {
         print_solution(&formula, solution);
         if (verify_solution(&formula, solution) == 1)
         {
-            printf("verify success");
+            printf("verify success\n");
         }
         else
         {
-            printf("verify fail");
+            printf("verify fail\n");
         }
     }
     else
     {
-        printf("error");
+        printf("error\n");
     }
-    system("pause");
     free_formula(&formula);
     free(solution);
+    end = clock();
+    cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("Time used: %f seconds\n", cpu_time_used);
+    system("pause");
     return 0;
 }
